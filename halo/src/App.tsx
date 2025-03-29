ff647f9daecef61820f470492b6be961614d868b
emmaazzi <eazzi2021@gmail.com>
2025-03-29 19:44:35 -0400

    feat: implement responsive navbar with SOS button and logo

--- halo/src/App.tsx
+++ halo/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import LandingPage from './pages/LandingPage'
+import LandingPage from './pages/LandingPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path='/landing' element={<LandingPage />} />
+        <Route path="/landing" element={<LandingPage />} />
       </Routes>
     </Router>
   );
